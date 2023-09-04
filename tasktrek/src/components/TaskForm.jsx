1ad33b98eabff8740baa30aea0c4237cdca5305a
dakota831 <drtorres831@gmail.com>
2023-09-04 18:36:50 -0400

    Finished TaskTrek App

--- tasktrek/src/components/TaskForm.jsx
+++ tasktrek/src/components/TaskForm.jsx
@@ -1,35 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TaskForm.css";
+import Tag from "./Tag";
 
-const TaskForm = () => {
-  return (
-    <header className="app_header">
-      <form>
-        <input type="text" className="task_input" placeholder="Add a task" />
+const TaskForm = ({ setTasks }) => {
+    const [taskData, setTaskData] = useState({
+        task: "",
+        status: "todo",
+        tags: [],
+    });
 
-        <div className="task_form_bottom_line">
-          <div>
-            <button className="tag">HTML</button>
-            <button className="tag">CSS</button>
-            <button className="tag">JAVASCRIPT</button>
-            <button className="tag">REACT</button>
-          </div>
+    const checkTag = (tag) => {
+        return taskData.tags.some((item) => item === tag);
+    };
 
-          <div>
-            <select className="task_status">
-              <option value="todo">To Do</option>
-              <option value="inprogress">In Progress</option>
-              <option value="done">Done</option>
-            </select>
-            <button type="submit" className="task_submit">
-              Add Task
-            </button>
-          </div>
-        </div>
-      </form>
-    </header>
-  );
+    const selectTag = (tag) => {
+        if (taskData.tags.some((item) => item === tag)) {
+            const filterTags = taskData.tags.filter((item) => item !== tag);
+            setTaskData((prev) => {
+                return { ...prev, tags: filterTags };
+            });
+        } else {
+            setTaskData((prev) => {
+                return { ...prev, tags: [...prev.tags, tag] };
+            });
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setTaskData((prev) => {
+            return { ...prev, [name]: value };
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(taskData);
+        setTasks((prev) => {
+            return [...prev, taskData];
+        });
+        setTaskData({
+            task: "",
+            status: "todo",
+            tags: [],
+        });
+    };
+    return (
+        <header className='app_header'>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='task'
+                    value={taskData.task}
+                    className='task_input'
+                    placeholder='Enter your task'
+                    onChange={handleChange}
+                />
+
+                <div className='task_form_bottom_line'>
+                    <div>
+                        <Tag
+                            tagName='HTML'
+                            selectTag={selectTag}
+                            selected={checkTag("HTML")}
+                        />
+                        <Tag
+                            tagName='CSS'
+                            selectTag={selectTag}
+                            selected={checkTag("CSS")}
+                        />
+                        <Tag
+                            tagName='JavaScript'
+                            selectTag={selectTag}
+                            selected={checkTag("JavaScript")}
+                        />
+                        <Tag
+                            tagName='React'
+                            selectTag={selectTag}
+                            selected={checkTag("React")}
+                        />
+                    </div>
+
+                    <div>
+                        <select
+                            name='status'
+                            value={taskData.status}
+                            className='task_status'
+                            onChange={handleChange}>
+                            <option value='todo'>To do</option>
+                            <option value='doing'>Doing</option>
+                            <option value='done'>Done</option>
+                        </select>
+                        <button type='submit' className='task_submit'>
+                            + Add Task
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </header>
+    );
 };
 
 export default TaskForm;
