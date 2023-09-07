1842adfb972b118de444f9ddb50e5c0789af965d
dakota831 <drtorres831@gmail.com>
2023-09-06 22:27:18 -0400

    MovieManiac App Update 1

--- MovieManiac/moviemaniac/src/components/MovieList/MovieList.jsx
+++ MovieManiac/moviemaniac/src/components/MovieList/MovieList.jsx
@@ -1,33 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MovieList.css";
 import Fire from "../../assets/fire.png";
+import MovieCard from "../MovieList/MovieCard";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]); // [movies, setMovies
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async () => {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=f0dda4100d00f65c02d8d3809dd4ea44"
+    );
+    const data = await response.json();
+    setMovies(data.results);
+  };
+
   return (
-    <section className="movie-list">
-      <header className="movie-list__header">
-        <h2 className="movie_list_heading">
+    <section className="movie_list">
+      <header className="align_center movie_list_header">
+        <h2 className="align_center movie_list_heading">
           Popular <img src={Fire} alt="fire emoji" className="navbar_emoji" />
         </h2>
 
-        <div className="movie-list_fs">
-          <ul className="movie_filter">
-            <li className="movie_filter_item">8+ Star</li>
+        <div className="align_center movie-list_fs">
+          <ul className="align_center movie_filter">
+            <li className="movie_filter_item active">8+ Star</li>
             <li className="movie_filter_item">7+ Star</li>
             <li className="movie_filter_item">6+ Star</li>
           </ul>
 
           <select name="" id="" className="movie_sorting">
-            <options>SortBy</options>
-            <options>Date</options>
-            <options>Rating</options>
+            <option value="">SortBy</option>
+            <option value="">Date</option>
+            <option value="">Rating</option>
           </select>
           <select name="" id="" className="movie_sorting">
-            <options>Ascending</options>
-            <options>Descending></options>
+            <option value="">Ascending</option>
+            <option value="">Descending</option>
           </select>
         </div>
       </header>
+      <div className="movie_cards">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </section>
   );
 };
