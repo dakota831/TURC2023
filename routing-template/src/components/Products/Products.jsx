75ddeb2e2ed7cfb60dd9d94662c7ffc8966f8350
dakota831 <drtorres831@gmail.com>
2023-09-09 14:46:18 -0400

    Creating Simple Routing

---
+++ routing-template/src/components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Products from './Products';
+
+const Products = () => {
+    return (
+        <div>
+            <h2>Products</h2>
+
+            ul>(li>Link[to="/Products"])*3
+        </div>
+    );
+};
+
+export default Products;
