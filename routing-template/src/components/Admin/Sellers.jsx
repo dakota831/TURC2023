cbdac886be366f3e97d9a4e12d911aff08ac5478
dakota831 <drtorres831@gmail.com>
2023-09-10 15:00:29 -0400

    Routing Project Complete

--- routing-template/src/components/Admin/Sellers.jsx
+++ routing-template/src/components/Admin/Sellers.jsx
@@ -1,42 +1,90 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Loader from "../common/loader";
+import apiClient from "../../utils/api-client";
+import Loader from "../Common/Loader";
 
-const sellers = () => {
-  const [name, setName] = React.useState("");
+const Sellers = () => {
+  const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [errors, setErrors] = useState(null);
-  const [sellers, setSellers] = React.useState([]);
+  const [errors, setErrors] = useState("");
+  const [sellers, setSellers] = useState([]);
 
   useEffect(() => {
+    // fetchSellers();
     setIsLoading(true);
-    axios
-      .get("https://jsonplaceholder.typicode.com/users")
+    apiClient
+      .get("/users")
       .then((res) => {
         setSellers(res.data);
         setIsLoading(false);
       })
       .catch((err) => {
-        setErrors(err.message);
         setIsLoading(false);
+        setErrors(err.message);
       });
   }, []);
+
+  const addSeller = () => {
+    const newSeller = {
+      name,
+      id: sellers.length + 1,
+    };
+    setSellers([newSeller, ...sellers]);
+
+    apiClient
+      .post("/users", newSeller)
+      .then((res) => setSellers([res.data, ...sellers]))
+      .catch((err) => {
+        setErrors(err.message);
+        setSellers(sellers);
+      });
+  };
+
+  const deleteSeller = (id) => {
+    setSellers(sellers.filter((s) => s.id !== id));
+    apiClient.delete(`/users/${id}`).catch((err) => {
+      setErrors(err.message);
+      setSellers(sellers);
+    });
+  };
+
+  const updateSeller = (seller) => {
+    const updatedSeller = {
+      ...seller,
+      name: seller.name + " Updated",
+    };
+    setSellers(sellers.map((s) => (s.id === seller.id ? updatedSeller : s)));
+
+    apiClient.patch(`/users/${seller.id}`, updatedSeller).catch((err) => {
+      setErrors(err.message);
+      setSellers(sellers);
+    });
+  };
 
   return (
     <>
       <h3>Admin Sellers Page</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} />
-      {isLoading && (
-        <div>
-          <Loader />
-          {errors && <em>{errors}</em>}
-        </div>
-      )}
-      {sellers.map((seller) => (
-        <p key={seller.id}>{seller.name}</p>
-      ))}
+      <button onClick={addSeller}>Add Seller</button>
+      {isLoading && <Loader />}
+      {errors && <em>{errors}</em>}
+
+      <table>
+        <tbody>
+          {sellers.map((seller) => (
+            <tr key={seller.id}>
+              <td>{seller.name}</td>
+              <td>
+                <button onClick={() => updateSeller(seller)}>Update</button>
+              </td>
+              <td>
+                <button onClick={() => deleteSeller(seller.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
 
-export default sellers;
+export default Sellers;
